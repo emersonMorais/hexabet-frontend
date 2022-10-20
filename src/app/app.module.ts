6a7839bc70ccea4aafe768ca6f9a981b38580b9b
Emerson <emerson_c.morais@hotmail.com>
2022-10-20 02:45:01 -0300

    add initial dashboard's structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { HomeModule } from './home/home.module';
-import { HomeRoutingModule } from './home/home-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeModule } from './home/home.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
